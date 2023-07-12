d2b16895f1210527cb2ef176631ee62a89cc03dd
iuhmirza <iuhmirza@gmail.com>
2023-07-12 12:59:26 +0100

    Create basic chat app scaffold

--- App.tsx
+++ App.tsx
@@ -1,15 +1,53 @@
-import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, {useState, useRef} from 'react';
+import { Button, FlatList, ScrollView, Text, TextInput, View } from "react-native";
+
+type UserProps = {
+  name: string;
+};
+
+function Chat(props: UserProps) {
+  const [messages, setMessages] = useState<string[]>([]);
+  const inputRef = useRef<TextInput>(null);
+  return (
+    <>
+      <Text>Wifi Chat Client - {props.name}</Text>
+      <FlatList
+        data={messages}
+        renderItem={item => <Text>{item.item}</Text>}
+      />
+      <View>
+        <TextInput
+          focusable={true}
+          placeholder={'Message'}
+          ref={inputRef}
+          onSubmitEditing={event => {
+            if (event.nativeEvent.text === '') return;
+            setMessages([...messages, event.nativeEvent.text]);
+            inputRef.current?.clear();
+          }}
+        />
+      </View>
+    </>
+  );
+}
 
 function App() {
-  return (
-    <ScrollView>
-      <Text>Wifi Chat Client</Text>
+  const [username, setUsername] = useState('');
+
+  if (username === '') {
+    return (
       <View>
-        <Text>What's good?</Text>
+        <Text>Enter your username:</Text>
+        <TextInput
+          placeholder={'Username'}
+          onSubmitEditing={event => setUsername(event.nativeEvent.text)}
+        />
+        <Button title={'Connect'} />
       </View>
-    </ScrollView>
-  );
+    );
+  }
+
+  return <Chat name={username} />;
 }
 
 export default App;
